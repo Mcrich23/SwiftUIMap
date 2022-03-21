4b9f3f8c2873be26112f4a460e102f168a21ee56
Morris Richman <mcrich23@icloud.com>
2022-03-20 23:05:25 -0700

    Bug Fixes

--- Sources/SwiftUIMap/SwiftUIMap.swift
+++ Sources/SwiftUIMap/SwiftUIMap.swift
@@ -114,8 +114,8 @@ public struct ExistingAnnotationMap: UIViewRepresentable {
             if mapView.selectedAnnotations.count > 0 {
                 mapView.deselectAnnotation(view as? MKAnnotation, animated: true)
             }
-//            if points != [] && points != nil {
-            let annotation = entireMapViewController.points.first { Annotations in
+//            if points != [] {
+                let annotation = entireMapViewController.points.first { Annotations in
                     Annotations.title == view.annotation?.title
                 }!
                 print("tapped annotation, annotation = \(annotation)")
@@ -138,6 +138,8 @@ public struct ExistingAnnotationMap: UIViewRepresentable {
                     entireMapViewController.selected(annotation, false)
                     entireMapViewController.address = annotation.address
                 }
+//            }else {
+//                print("no annotation")
 //            }
         }
         public func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
