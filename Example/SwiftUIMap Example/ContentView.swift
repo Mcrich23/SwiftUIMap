88a0853e514c2fa859e191f4fe9cf9585d1d968f
Morris Richman <mcrich23@icloud.com>
2022-03-21 09:31:34 -0700

    Update ContentView.swift

--- Example/SwiftUIMap Example/ContentView.swift
+++ Example/SwiftUIMap Example/ContentView.swift
@@ -11,7 +11,7 @@ import MapKit
 
 struct ContentView: View {
     var body: some View {
-        ExistingAnnotationMap(zoom: 0.4, address: "Seattle, Wa", points: [Annotations(title: "Test", subtitle: "", address: "1119 8th Ave, Seattle, WA, 98101, United States", glyphImage: "", tintColor: .orange, markerTintColor: .orange, glyphTintColor: .white, displayPriority: .required), Annotations(title: "Test", subtitle: "", address: "Ballard, Seattle, Wa", glyphImage: "", tintColor: .orange, markerTintColor: .orange, glyphTintColor: .white, displayPriority: .required)], pointsOfInterestFilter: .excludingAll) { Title, Subtitle, Address, Cluster  in
+        ExistingAnnotationMap(zoom: 0.2, address: "Seattle, Wa", points: [Annotations(title: "Townhall", subtitle: "Newly Remodeled", address: "1119 8th Ave, Seattle, WA, 98101, United States", glyphImage: "", tintColor: .orange, markerTintColor: .orange, glyphTintColor: .white, displayPriority: .required)], pointsOfInterestFilter: .excludingAll) { Title, Subtitle, Address, Cluster  in
             print("tapped \(Address)")
         } deselected: {
             print("deselected annotation")
