8a36325b356ea580c79cc4221502a8c99500ad58
Morris Richman <mcrich23@icloud.com>
2022-03-20 21:04:52 -0700

    Bug Fixes

--- Sources/SwiftUIMap/Data Structs.swift
+++ Sources/SwiftUIMap/Data Structs.swift
@@ -14,9 +14,20 @@ public struct Annotations: Identifiable, Equatable {
     public let title: String
     public let subtitle: String
     public let address: String
+    public let glyphImage: String
     public let tintColor: Color
     public let markerTintColor: Color
     public let glyphTintColor: Color
-    public let glyphImage: String
     public let displayPriority: MKFeatureDisplayPriority
+    
+//    init(title: String, subtitle: String, address: String, glyphImage: String, tintColor: Color, markerTintColor: Color, glyphTintColor: Color, displayPriority: MKFeatureDisplayPriority) {
+//        self.title = title
+//        self.subtitle = subtitle
+//        self.address = address
+//        self.glyphImage = glyphImage
+//        self.tintColor = tintColor
+//        self.markerTintColor = markerTintColor
+//        self.glyphTintColor = glyphTintColor
+//        self.displayPriority = displayPriority
+//    }
 }
