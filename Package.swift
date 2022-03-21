fd8423a3a6e909452f80e66b4e4e44c6b4dd9195
Morris Richman <mcrich23@icloud.com>
2022-03-21 09:36:50 -0700

    Bug Fixes

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SwiftUIMap",
-            targets: ["SwiftUIMap"]),
+            targets: ["Existing Annotation Map", "Mutatable Annotation Map"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
