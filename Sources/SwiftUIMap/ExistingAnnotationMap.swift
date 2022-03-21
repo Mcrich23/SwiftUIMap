d254dc677bb371865d47b2d562c9aca27dca1393
Morris Richman <mcrich23@icloud.com>
2022-03-21 00:17:15 -0700

    Bug Fixes

--- Sources/SwiftUIMap/ExistingAnnotationMap.swift
+++ Sources/SwiftUIMap/ExistingAnnotationMap.swift
@@ -122,7 +122,7 @@ struct rawExistingAnnotationMap: UIViewRepresentable {
                     // Use your location
                     location.coordinate.latitude == view.annotation?.coordinate.latitude && location.coordinate.longitude == view.annotation?.coordinate.longitude
                 }
-            })!
+            })
 //            let annotation = annotations.first!
             print("tapped annotation, annotation = \(annotation)")
             if let cluster = view.annotation as? MKClusterAnnotation {
@@ -132,16 +132,16 @@ struct rawExistingAnnotationMap: UIViewRepresentable {
                 // If you want the map to display the cluster members
                 if arrayList.count > 1 {
                     entireMapViewController.zoom = entireMapViewController.zoom/3
-                    entireMapViewController.address = annotation.address
+                    entireMapViewController.address = annotation!.address
                     print("zoom = \(entireMapViewController.zoom)")
-                    entireMapViewController.selected(annotation, true)
+                    entireMapViewController.selected(annotation!, true)
                 }else {
-                    entireMapViewController.selected(annotation, false)
-                    entireMapViewController.address = annotation.address
+                    entireMapViewController.selected(annotation!, false)
+                    entireMapViewController.address = annotation!.address
                 }
             }else {
-                entireMapViewController.selected(annotation, false)
-                entireMapViewController.address = annotation.address
+                entireMapViewController.selected(annotation!, false)
+                entireMapViewController.address = annotation!.address
             }
             //            }else {
             //                print("no annotation")
