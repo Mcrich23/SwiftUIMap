16115ce9031af66dd3dd23a68658ff902e8c1c0f
Morris Richman <mcrich23@icloud.com>
2022-03-23 18:59:17 -0700

    Bug Fixes

--- Example/SwiftUIMap Example/ContentView.swift
+++ Example/SwiftUIMap Example/ContentView.swift
@@ -12,9 +12,9 @@ import MapKit
 struct ContentView: View {
     var body: some View {
         ExistingAnnotationMap(zoom: 0.2, address: "Seattle, Wa", points: [
-            Annotations(title: "Townhall", subtitle: "Newly Remodeled", address: "1119 8th Ave, Seattle, WA, 98101, United States", glyphImage: "", tintColor: .orange, markerTintColor: .orange, glyphTintColor: .white, displayPriority: .required),
-            Annotations(title: "Space Needle", subtitle: "", address: "400 Broad St Seattle, WA 98109, United States", glyphImage: "", tintColor: .red, markerTintColor: .red, glyphTintColor: .white, displayPriority: .required),
-            Annotations(title: "Pike Place Market", subtitle: "", address: "85 Pike St Seattle, WA  98101, United States", glyphImage: "", tintColor: .red, markerTintColor: .red, glyphTintColor: .white, displayPriority: .required)
+            Annotations(title: "Townhall", subtitle: "Newly Remodeled", address: "1119 8th Ave, Seattle, WA, 98101, United States", glyphImage: UIImage(systemName: "building.columns"), tintColor: .orange, markerTintColor: .orange, glyphTintColor: .white, displayPriority: .required),
+            Annotations(title: "Space Needle", subtitle: "", address: "400 Broad St Seattle, WA 98109, United States", glyphImage: UIImage(), tintColor: .red, markerTintColor: .red, glyphTintColor: .white, displayPriority: .required),
+            Annotations(title: "Pike Place Market", subtitle: "", address: "85 Pike St Seattle, WA  98101, United States", glyphImage: UIImage(systemName: "shopping_basket"), tintColor: .red, markerTintColor: .red, glyphTintColor: .white, displayPriority: .required)
         ], pointsOfInterestFilter: .excludingAll) { Title, Subtitle, Address, Cluster  in
             print("tapped \(Address)")
         } deselected: {
