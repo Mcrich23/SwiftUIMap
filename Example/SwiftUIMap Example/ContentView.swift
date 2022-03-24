c82372f438420acf999aff35d11d6d28168851c8
Morris Richman <mcrich23@icloud.com>
2022-03-23 20:24:35 -0700

    Added Promo Image

--- Example/SwiftUIMap Example/ContentView.swift
+++ Example/SwiftUIMap Example/ContentView.swift
@@ -12,7 +12,7 @@ import MapKit
 struct ContentView: View {
     var body: some View {
         ExistingAnnotationMap(zoom: 0.2, address: "Seattle, Wa", points: [
-            Annotations(title: "Townhall", subtitle: "Newly Remodeled", address: "1119 8th Ave, Seattle, WA, 98101, United States", glyphImage: .systemImage("building.columns"), tintColor: .systemGray, markerTintColor: .systemGray, glyphTintColor: .white, displayPriority: .required),
+            Annotations(title: "Seattle Town Hall", subtitle: "Newly Remodeled", address: "1119 8th Ave, Seattle, WA, 98101, United States", glyphImage: .systemImage("building.columns"), tintColor: .systemGray, markerTintColor: .systemGray, glyphTintColor: .white, displayPriority: .required),
             Annotations(title: "Space Needle", subtitle: "", address: "400 Broad St Seattle, WA 98109, United States", glyphImage: .systemImage("star.fill"), tintColor: .systemPurple, markerTintColor: .systemPurple, glyphTintColor: .white, displayPriority: .required),
             Annotations(title: "Pike Place Market", subtitle: "", address: "85 Pike St Seattle, WA  98101, United States", glyphImage: .systemImage("cart"), tintColor: .systemOrange, markerTintColor: .systemOrange, glyphTintColor: .white, displayPriority: .required)
         ], pointsOfInterestFilter: .excludingAll) { Title, Subtitle, Address, Cluster  in
