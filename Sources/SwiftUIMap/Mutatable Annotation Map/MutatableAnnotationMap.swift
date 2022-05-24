a575234af91c2fde36179a506ef3c833271c65b4
Morris Richman <mcrich23@icloud.com>
2022-05-23 18:03:22 -0700

    Added documentation to modifiers

--- Sources/SwiftUIMap/Mutatable Annotation Map/MutatableAnnotationMap.swift
+++ Sources/SwiftUIMap/Mutatable Annotation Map/MutatableAnnotationMap.swift
@@ -164,74 +164,236 @@ public struct MutableMapView: View {
         rawMutableAnnotationMap(zoom: zoom, address: address, modifierMap: modifierMap)
     }
     // MARK: Modifiers
+    /**
+    Select the pointOfInterestCategories
+    - parameter include: An array including all the point of interest types you want to show on your map.
+     
+    # Example #
+    ```
+     .pointOfInterestCategories(include: [.airport])
+    ```
+    */
     public func pointOfInterestCategories(include points: [MKPointOfInterestCategory]) -> MutableMapView {
         modifierMap.pointOfInterestFilter = MKPointOfInterestFilter(including: points)
         return self
     }
+    /**
+    Select the pointOfInterestCategories
+    - parameter include: An array excluding all the point of interest types you don't want to show on your map.
+     
+    # Example #
+    ```
+     .pointOfInterestCategories(exclude: [.airport])
+    ```
+    */
     public func pointOfInterestCategories(exclude points: [MKPointOfInterestCategory]) -> MutableMapView {
         modifierMap.pointOfInterestFilter = MKPointOfInterestFilter(excluding: points)
         return self
     }
-    public func pointsOfInterest(_ filter: MKPointOfInterestFilter!) -> MutableMapView {
+    /**
+    Select the pointsOfInterestFilter
+    - parameter filter: A class that allows you to include or exclude all points of interest
+     
+    # Example #
+    ```
+     .pointsOfInterest(.includeAll)
+    ```
+    */
+    public func pointsOfInterest(_ filter: MKPointOfInterestFilter) -> MutableMapView {
         modifierMap.pointOfInterestFilter = filter
         return self
     }
+    /**
+    Whether you want the compass to show.
+    - parameter show: A boolean for if you want to show the compass or not.
+     
+    # Example #
+    ```
+     .showCompass(true)
+    ```
+    */
     public func showCompass(_ show: Bool) -> MutableMapView {
         modifierMap.showsCompass = show
         return self
     }
+    /**
+    Whether you want the scale to show.
+    - parameter show: A boolean for if you want to show the scale or not.
+     
+    # Example #
+    ```
+     .showScale(true)
+    ```
+    */
     public func showScale(_ show: Bool) -> MutableMapView {
         modifierMap.showsScale = show
         return self
     }
+    /**
+    Whether you want to show the traffic.
+    - parameter show: A boolean for if you want to show the traffic or not.
+     
+    # Example #
+    ```
+     .showTraffic(true)
+    ```
+    */
     public func showTraffic(_ show: Bool) -> MutableMapView {
         modifierMap.showsTraffic = show
         return self
     }
+    /**
+    Whether you want to show the buildings.
+    - parameter show: A boolean for if you want to show the buildings or not.
+     
+    # Example #
+    ```
+     .showTraffic(true)
+    ```
+    */
     public func showBuildings(_ show: Bool) -> MutableMapView {
         modifierMap.showsBuildings = show
         return self
     }
+    /**
+    What type of map you show.
+    - parameter type: The type of map that is shown.
+     
+    # Example #
+    ```
+     .mapType(.standard)
+    ```
+    */
     public func mapType(_ type: MKMapType) -> MutableMapView {
         modifierMap.mapType = type
         return self
     }
+    /**
+    Settings for the camera angle.
+    - parameter camera: A `MKMapCamera` variable with all the settings for the camera viewing the map .
+     
+    # Example #
+    ```
+     .camera(MKMapCamera(lookingAtCenter: loc, fromDistance: 4, pitch: 4, heading: 4))
+    ```
+    */
     public func camera(_ camera: MKMapCamera) -> MutableMapView {
         modifierMap.camera = camera
         return self
     }
+    /**
+    Whether or not zoom is enabled.
+    - parameter enabled: A boolean for if you want zoom to be enabled.
+     
+    # Example #
+    ```
+     .isZoomEnabled(true)
+    ```
+    */
     public func isZoomEnabled(_ enabled: Bool) -> MutableMapView {
         modifierMap.isZoomEnabled = enabled
         return self
     }
+    /**
+     A boundary of an area within which the map's center must remain.
+    - parameter boundary: The camera boundary and all the settings that are availible.
+     
+    # Example #
+    ```
+     .cameraBoundary(MKMapView.CameraBoundary(coordinateRegion: MKCoordinateRegion(center: loc, span: MKCoordinateSpan(latitudeDelta: 4, longitudeDelta: 4))))
+    ```
+    */
     public func cameraBoundary(_ boundary: MKMapView.CameraBoundary?) -> MutableMapView {
         modifierMap.cameraBoundary = boundary
         return self
     }
+    /**
+     Select the zoom range for your map.
+    - parameter range: The camera range and all the settings that are availible.
+     
+    # Example #
+    ```
+     .cameraZoomRange(MKMapView.CameraZoomRange(minCenterCoordinateDistance: CLLocationDistance(600)))
+    ```
+    */
     public func cameraZoomRange(_ range: MKMapView.CameraZoomRange!) -> MutableMapView {
         modifierMap.cameraZoomRange = range
         return self
     }
+    /**
+    Whether or not pitch is enabled.
+    - parameter enabled: A boolean for if you want pitch to be enabled.
+     
+    # Example #
+    ```
+     .isPitchEnabled(true)
+    ```
+    */
     public func isPitchEnabled(_ enabled: Bool) -> MutableMapView {
         modifierMap.isPitchEnabled = enabled
         return self
     }
+    /**
+    Whether or not rotate is enabled.
+    - parameter enabled: A boolean for if you want rotate to be enabled.
+     
+    # Example #
+    ```
+     .isRotateEnabled(true)
+    ```
+    */
     public func isRotateEnabled(_ enabled: Bool) -> MutableMapView {
         modifierMap.isRotateEnabled = enabled
         return self
     }
+    /**
+    Whether or not scroll is enabled.
+    - parameter enabled: A boolean for if you want scroll to be enabled.
+     
+    # Example #
+    ```
+     .isScrollEnabled(true)
+    ```
+    */
     public func isScrollEnabled(_ enabled: Bool) -> MutableMapView {
         modifierMap.isScrollEnabled = enabled
         return self
     }
+    /**
+    Whether or not multiple touch is enabled.
+    - parameter enabled: A boolean for if you want multiple touch to be enabled.
+     
+    # Example #
+    ```
+     .isMultipleTouchEnabled(true)
+    ```
+    */
     public func isMultipleTouchEnabled(_ enabled: Bool) -> MutableMapView {
         modifierMap.isMultipleTouchEnabled = enabled
         return self
     }
+    /**
+    Whether or not user interaction is enabled.
+    - parameter enabled: A boolean for if you want user interaction to be enabled.
+     
+    # Example #
+    ```
+     .isUserInteractionEnabled(true)
+    ```
+    */
     public func isUserInteractionEnabled(_ enabled: Bool) -> MutableMapView {
         modifierMap.isUserInteractionEnabled = enabled
         return self
     }
+    /**
+    The type of user tracking mode shown on the map.
+    - parameter mode: Decide the user tracking mode for the map.
+     
+    # Example #
+    ```
+     .userTrackingMode(.follow)
+    ```
+    */
     public func userTrackingMode(_ mode: MKUserTrackingMode) -> MutableMapView {
         modifierMap.userTrackingMode = mode
         return self
